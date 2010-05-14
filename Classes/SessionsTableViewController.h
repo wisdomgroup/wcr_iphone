41019df98e65ad47380577ed34d0f9b573abc784
Stanley Fisher <stanley.fisher@gmail.com>
2010-05-14 15:53:07 -0500

    Added Sessions table view

---
+++ Classes/SessionsTableViewController.h
@@ -0,0 +1,16 @@
+//
+//  SessionsTableViewController.h
+//  WindyCityDB
+//
+//  Created by Stanley Fisher on 5/14/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface SessionsTableViewController : UITableViewController {
+
+}
+
+@end

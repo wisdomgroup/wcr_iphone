0af223c5b9e5499567b6fc04a5b9e56bfe73be09
Stanley Fisher <stanley.fisher@gmail.com>
2010-05-15 00:47:47 -0500

    Added navigation bar to Sessions tab and added Map view tab

---
+++ Classes/MapViewController.h
@@ -0,0 +1,16 @@
+//
+//  MapViewController.h
+//  WindyCityDB
+//
+//  Created by Stanley Fisher on 5/14/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface MapViewController : UIViewController {
+
+}
+
+@end

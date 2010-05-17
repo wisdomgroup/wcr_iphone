352d89d8a93667600adf99d768efdf2bb070ceb2
RayHightower <rth@wisdomgroup.com>
2010-05-17 09:04:25 -0500

    Minor modifications to samples.

--- _WCDB_Sample_Code/RandomController.h
+++ _WCDB_Sample_Code/RandomController.h
@@ -2,7 +2,7 @@
 //  RandomController.h
 //  WindyCityDB
 //
-//  Created by as a placeholder for sample code.
+//  Created by as a placeholder for sample code. Feel free to modify at will. 
 //  Copyright 2010 __MyCompanyName__. All rights reserved.
 //
 

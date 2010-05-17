31514e4dd5f87aaa0f26578e33022b83aee97d3c
Stanley Fisher <stanley.fisher@gmail.com>
2010-05-16 23:48:51 -0500

    Added a tab to show the sponsors

---
+++ Classes/SponsorsTableViewController.h
@@ -0,0 +1,16 @@
+//
+//  SponsorsTableViewController.h
+//  WindyCityDB
+//
+//  Created by Stanley Fisher on 5/16/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface SponsorsTableViewController : UITableViewController {
+
+}
+
+@end

0839c52691d2aa1d86eccda0ce6f8314ca2337a3
Stanley Fisher <stanley.fisher@gmail.com>
2010-06-06 00:09:54 -0500

    Change view of venue details

--- Classes/MapViewController.h
+++ Classes/MapViewController.h
@@ -9,15 +9,18 @@
 #import <UIKit/UIKit.h>
 #import <MapKit/MapKit.h>
 
-#import "VenueDetailViewController.h"
+//#import "VenueDetailViewController.h"
+#import "VenueDetailTableViewController.h"
 
 
 @interface MapViewController : UIViewController <MKMapViewDelegate> {
     MKMapView *mapView;
-    VenueDetailViewController *venueDetailViewController;
+    //VenueDetailViewController *venueDetailViewController;
+    VenueDetailTableViewController *venueDetailViewController;
 }
 
 @property (nonatomic, retain) IBOutlet MKMapView *mapView;
-@property (nonatomic, retain) IBOutlet VenueDetailViewController *venueDetailViewController;
+//@property (nonatomic, retain) IBOutlet VenueDetailViewController *venueDetailViewController;
+@property (nonatomic, retain) IBOutlet VenueDetailTableViewController *venueDetailViewController;
 
 @end
---
+++ Classes/VenueDetailTableViewController.h
@@ -0,0 +1,16 @@
+//
+//  VenueDetailTableViewController.h
+//  WindyCityDB
+//
+//  Created by Stanley Fisher on 6/5/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface VenueDetailTableViewController : UITableViewController {
+
+}
+
+@end
